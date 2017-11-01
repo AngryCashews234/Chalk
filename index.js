98ba4f36bbe94471f7dc06c767f90d3b68145ef3
AngryCashews234 <33270165+AngryCashews234@users.noreply.github.com>
2017-10-31 19:34:12 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const chalk = require('chalk');
+
+console.log(chalk.green.bold("Best Friends Forever"));
+console.log(" ");
+console.log(chalk.red("    There once was a boy named Nathan who had a little puppy named Max.\
+ One-day Max and Nathan went for a walk in the park, during the time Max smelled the sweet \
+ deliciousness of hamburgers in a food truck driving down the highway. Max ran off into a crowd\
+ of people chasing the food truck. In a panic, Nathan sprints into motion chasing Max, but he \
+disappears within the crowd. Nathan asked several of the people during the notion if they have\
+seen Max, but no one knew where he went. Once Nathan returned home his parents and him went\
+ into action, getting the word out about Max disappearance. They put up flayers around the town, \
+ called the animal shelters, and even called the police if they have seen a golden retriever around. \
+ Weeks and months went by and no one has said word about the whereabouts of Max. "));
+
+ console.log(chalk.blue("   Walking in the neighbored one day, a young girl named Kaylee saw a flyer\
+ of Max on a telephone pole. She said to herself in astonishment 'I know that dog!' Then out of\
+ the corner of her eye, she saw max digging through the trash, looking for food. She ran over to\
+the dog and yelled Come here Max! With enjoyment Max came running over, jumped on her and \
+licking her face. Kaylee put Max into her car and drove to Nathan’s house. At his house, Nathan \
+heard the doorbell ring and went to go see who was there. In shock he saw both Max and Kaylee \
+there. Kaylee said with sarcasm, I think he is yours? and chuckled.  Nathan gave Max the biggest\
+hug in the world and said, thank you so much Kaylee, it’s been forever since I saw you. Kaylee\
+replied, I know that’s what happens when you move away for college. They both laughed and went \
+on inside. They became caught up with each other about old times of being in high school and the\
+ funny stuff they used to do. Nathan looked at Kaylee and said, Best Friends? Kaylee replied,\
+ Yeah and this time forever."));
